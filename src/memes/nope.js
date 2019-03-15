85f0ccbd4fe4d4557e1a0e2c3858be618f12a4a9
Martín Lombardo <mlomb@users.noreply.github.com>
2019-03-15 14:05:47 -0300

    nope update

--- src/memes/nope.js
+++ src/memes/nope.js
@@ -1,9 +1,12 @@
-var GiphyPreset = require('../GiphyPreset');
+import GiphyPreset from '../GiphyPreset';
+import RainPreset from '../RainPreset';
 
-module.exports = {
-	title: "NOPE NOPE NOPE",
-	variants: [
-		new GiphyPreset('6h4z4b3v6XWxO'),
-		new GiphyPreset('3ohs7KViF6rA4aan5u')
-	]
-};
+export let Variants = [
+	new GiphyPreset('6h4z4b3v6XWxO'),
+	new GiphyPreset('3ohs7KViF6rA4aan5u')
+	/*
+	new RainPreset(function() {
+		return `NOP` + (Math.random() > 0.5 ? 'E' : 'e');
+	})
+	*/
+];
