224dc7c0b40749c730bd4ffe5562cb8a711a91d7
Martín Lombardo <mlomb@users.noreply.github.com>
2019-03-15 13:09:29 -0300

    More functions

--- src/app.js
+++ src/app.js
@@ -3,4 +3,26 @@ var context = require.context('./memes', true, /\.(js)$/);
 var module = context(window.meme_module);
 var variant = module.variants[Math.floor(Math.random() * module.variants.length)];
 
-document.getElementById('root').innerHTML = variant.render();
+// init
+// render
+// ready
+// frame (each animation frame)
+
+if(typeof variant.init === 'function') {
+	variant.init();
+}
+if(typeof variant.render === 'function') {
+	document.getElementById('root').innerHTML = variant.render();
+}
+if(typeof variant.ready === 'function') {
+	variant.ready();
+}
+if(typeof variant.frame === 'function') {
+	var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+
+	function frame() {
+		variant.frame();
+		requestAnimationFrame(frame);
+	}
+	requestAnimationFrame(frame);
+}
