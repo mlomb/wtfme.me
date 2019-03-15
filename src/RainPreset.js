8ee5eb9cd5d8efcd61d9e1917454a9f854edb465
Martín Lombardo <mlomb@users.noreply.github.com>
2019-03-15 14:37:37 -0300

    RainPreset

--- src/RainPreset.js
+++ src/RainPreset.js
@@ -1,13 +1,47 @@
 import Preset from './Preset';
 
 export default class RainPreset extends Preset {
-	constructor(id) {
+	constructor(make) {
 		super();
+
+		this.drops = [];
+		this.make = make; // make should return a DOM element
+	}
+
+	createDrop() {
+		let raindrop = this.make();
+
+		raindrop.style.position = 'absolute';
+
+		raindrop.x = Math.random() * 1.2 - 0.1;
+		raindrop.y = Math.random() * 0.5 - 0.5;
+		raindrop.y_acc = 0.002 + Math.random() * 0.01;
+
+		this.container.appendChild(raindrop);
+		this.drops.push(raindrop);
 	}
 
 	init(root) {
 		this.container = document.createElement('div');
 
 		root.appendChild(this.container);
+	}
+
+	frame() {
+		var i = this.drops.length;
+		while(i--) {
+			let t = this.drops[i];
+			t.y += t.y_acc;
+			if(t.y >= 1) {
+				this.drops.splice(i, 1);
+				t.remove();
+			}
+		}
+		while(this.drops.length < 200)
+			this.createDrop();
+		for(let t of this.drops) {
+			t.style.left = (t.x * 100).toFixed(2) + '%';
+			t.style.top = (t.y * 100).toFixed(2) + '%';
+		}
 	}
 }
--- src/memes/nope.js
+++ src/memes/nope.js
@@ -3,10 +3,10 @@ import RainPreset from '../RainPreset';
 
 export let Variants = [
 	new GiphyPreset('6h4z4b3v6XWxO'),
-	new GiphyPreset('3ohs7KViF6rA4aan5u')
-	/*
+	new GiphyPreset('3ohs7KViF6rA4aan5u'),
 	new RainPreset(function() {
-		return `NOP` + (Math.random() > 0.5 ? 'E' : 'e');
+		let nope = document.createElement('span');
+		nope.innerText = `NOP` + (Math.random() > 0.5 ? 'E' : 'e');
+		return nope;
 	})
-	*/
 ];
