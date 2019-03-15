df6c28e0ec810dd1c5ea9a331f9d91e6f9321185
nMusacchio <musacchio.ignacio@gmail.com>
2019-03-15 15:03:26 -0300

    WindowsError

--- src/CanvasPreset.js
+++ src/CanvasPreset.js
@@ -1,21 +1,22 @@
 import Preset from './Preset';
 
 export default class CanvasPreset extends Preset {
-	constructor(draw) {
-		super();
-		this.draw = draw;
-	}
-
 	init(root) {
 		this.canvas = document.createElement('canvas');
 		this.ctx = this.canvas.getContext('2d');
+        this.mousePosition = {};
 
 		root.appendChild(this.canvas);
 
 		window.addEventListener('resize', this.resize.bind(this));
 		window.addEventListener('orientationchange', this.resize.bind(this));
+        window.addEventListener('mousemove', this.mousemove.bind(this));
 		this.resize();
 	}
+
+    mousemove(e) {
+        this.mousePosition = { x: e.pageX, y: e.pageY };
+    }
 
 	resize() {
 		this.canvas.width = window.innerWidth;
--- src/memes/windows.js
+++ src/memes/windows.js
@@ -1,15 +1,29 @@
 import CanvasPreset from '../CanvasPreset';
 
 class WindowsError extends CanvasPreset {
+    constructor(url){
+        super();
+        this.url = url;
+    }
+
+    init(root){
+        super.init(root);
+        this.img = new Image();
+        this.img.src = this.url;
+
+        var bg = document.createElement('div');
+		bg.classList.add("fullscreen-bg");
+        bg.style.backgroundImage = 'url(http://i.imgur.com/lojmr.jpg)'
+		root.prepend(bg);
+    }
 
 	frame() {
+        //super.frame();
 		const ctx = this.ctx;
-
-		ctx.fillStyle = 'red';
-		ctx.fillRect(0, 0, 150, 150);
+		ctx.drawImage(this.img, this.mousePosition.x - this.img.width / 2, this.mousePosition.y - this.img.height / 2);
 	}
 }
 
 export let Variants = [
-    new WindowsError(/* an image, for example */)
+    new WindowsError('https://i.imgur.com/jccR2lH.jpg')
 ];
