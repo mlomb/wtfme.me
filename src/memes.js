e79703e21739581d14bd6596704f4f048913d6df
Martín Lombardo <mlomb@users.noreply.github.com>
2019-03-15 16:06:24 -0300

    Meta tags

--- src/memes.js
+++ src/memes.js
@@ -1,12 +1,16 @@
 
 module.exports = [
 	{
-		title: 'NOPE NOPE NOPE',
+		title: 'nOpE',
+		description: 'NOPE NOPE NOPE NOPE NOPE',
+		keywords: ['nope'],
 		path: 'nope',
 		module: 'nope.js'
 	},
 	{
 		title: 'Windows error',
+		description: 'Windows errors on top of the Windows XP background',
+		keywords: ['window errors', 'windows xp', 'windows 7'],
 		path: 'windows',
 		module: 'windows.js'
 	}
