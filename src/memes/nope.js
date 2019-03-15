6fd68bde7e249c6ec146d0848c77c079bec151db
Martín Lombardo <mlomb@users.noreply.github.com>
2019-03-15 17:06:50 -0300

    nope rain with different casing

--- src/memes/nope.js
+++ src/memes/nope.js
@@ -6,7 +6,8 @@ export let Variants = [
 	new GiphyPreset('3ohs7KViF6rA4aan5u'),
 	new RainPreset(function() {
 		let nope = document.createElement('span');
-		nope.innerText = `NOP` + (Math.random() > 0.5 ? 'E' : 'e');
+		nope.style.opacity = Math.random() * 0.4 + 0.6;
+		nope.innerText = `${Math.random() > 0.5 ? 'N' : 'n'}${Math.random() > 0.5 ? 'O' : 'o'}${Math.random() > 0.5 ? 'P' : 'p'}${Math.random() > 0.5 ? 'E' : 'e'}`;
 		return nope;
 	})
 ];
