ee7b4edb4d012253324c68462dd66a00b2f154f4
Martín Lombardo <mlomb@users.noreply.github.com>
2019-03-15 16:53:42 -0300

    App message

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const context = require.context('./memes', true, /\.(js)$/);
 const module = context(`./${window.meme_module}`);
 const root = document.getElementById('root');
 
+console.log('%c Want to add more memes? https://github.com/mlomb/wtfme.me', 'background: #222; color: #bada55');
+
 if(module.Variants && module.Variants.length > 0) {
 	var variants = module.Variants[Math.floor(Math.random() * module.Variants.length)];
 
