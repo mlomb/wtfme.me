9ab890c06cf97dd7302eac016a7d9fd0a0e5460a
nMusacchio <musacchio.ignacio@gmail.com>
2019-03-15 22:45:36 -0300

    404

--- src/memes/404.js
+++ src/memes/404.js
@@ -1,5 +1,17 @@
 import {ImgurPreset} from '@presets/ImagePreset';
+import TextPreset from '@presets/TextPreset';
+
+var errorText = new TextPreset(`This animal is like the page you are looking for... <br> It doesn't exist`, 85);
 
 export let Variants = [
-	new ImgurPreset('tjF63dq')
+	[new ImgurPreset('tjF63dq'), errorText],
+	[new ImgurPreset('dCeSHc4'), errorText],
+	[new ImgurPreset('yZIGP0m'), errorText],
+	[new ImgurPreset('mmhM7xG'), errorText],
+	[new ImgurPreset('Hl0dQj2'), errorText],
+	[new ImgurPreset('lYrdRgq'), errorText],
+	[new ImgurPreset('AnOP69h'), errorText],
+	[new ImgurPreset('dWflG9S'), errorText],
+	[new ImgurPreset('ZbtgSMi'), errorText],
+	[new ImgurPreset('JtkZpfj'), errorText]
 ];
