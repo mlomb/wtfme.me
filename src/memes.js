8b0d04ea0c823df44cf9c4ad75e5b78133e0aa86
nMusacchio <musacchio.ignacio@gmail.com>
2019-03-15 23:02:16 -0300

    Index

--- src/memes.js
+++ src/memes.js
@@ -5,27 +5,31 @@ module.exports = [
 		description: '404 Not. Found.',
 		keywords: ['404', 'not found'],
 		path: '404',
-		module: '404.js'
+		module: '404.js',
+		cover: 'https://i.imgur.com/tjF63dqg.jpg'
 	},
 	{
 		title: 'nOpE',
 		description: 'NOPE NOPE NOPE NOPE NOPE',
 		keywords: ['no', 'nope'],
 		path: 'nope',
-		module: 'nope.js'
+		module: 'nope.js',
+		cover: 'https://i.ytimg.com/vi/8JaJzzNfygo/hqdefault.jpg'
 	},
 	{
 		title: 'Windows error',
 		description: 'Windows errors on top of the Windows XP background',
 		keywords: ['windows errors', 'windows xp', 'windows 7'],
 		path: 'windows',
-		module: 'windows.js'
+		module: 'windows.js',
+		cover: 'https://i.ytimg.com/vi/NBFMH-2i34Q/maxresdefault.jpg'
 	},
 	{
 		title: 'Enano asador',
 		description: 'Enano asador bostero',
 		keywords: ['asado', 'boca', 'enano'],
 		path: 'enanoasador',
-		module: 'enanoasador.js'
+		module: 'enanoasador.js',
+		cover: 'https://http2.mlstatic.com/enano-asador-D_NQ_NP_679438-MLA29211706073_012019-F.jpg'
 	}
 ];
