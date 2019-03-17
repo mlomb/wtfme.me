2e83acf61dc6b962dccb3c71d8af1ea09db7e31b
nMusacchio <musacchio.ignacio@gmail.com>
2019-03-17 14:53:25 -0300

    Teemo

--- src/memes/teemo.js
+++ src/memes/teemo.js
@@ -7,4 +7,7 @@ const deal = new GiphyPreset('Tte95WnDGq5Gg');
 export let Variants = [
 	[new SoundCloudPreset('591518472'), deal],
 	[new SoundCloudPreset('199105914'), deal],
+	[new SoundCloudPreset('591531243'), deal],
+	[new SoundCloudPreset('591531087'), deal],
+	[new SoundCloudPreset('591531159'), deal]
 ];
