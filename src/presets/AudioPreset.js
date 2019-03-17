62235410e8ecc9d272abda73c8ec3e86de413609
nMusacchio <musacchio.ignacio@gmail.com>
2019-03-17 14:26:17 -0300

    AudioPreset

--- src/presets/AudioPreset.js
+++ src/presets/AudioPreset.js
@@ -14,7 +14,7 @@ export default class AudioPreset extends Preset {
         iframe.setAttribute('frameborder', "no");
         iframe.setAttribute('allow', "autoplay");
         iframe.classList.add('audio');
-        iframe.src = this.url;
+        iframe.src = 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/'+this.url+'&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true';
         root.appendChild(iframe);
 	}
 }
