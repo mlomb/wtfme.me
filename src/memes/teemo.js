4df5839e9d88abf2271e5001bf1d7304f8a1e446
nMusacchio <musacchio.ignacio@gmail.com>
2019-03-17 14:49:31 -0300

    Teemo

--- src/memes/teemo.js
+++ src/memes/teemo.js
@@ -2,8 +2,8 @@ import AudioPreset from '@presets/AudioPreset';
 import CanvasPreset from '@presets/CanvasPreset.js';
 import {GiphyPreset} from '@presets/ImagePreset';
 
-var audios = ['591518472', '199105914'];
 
 export let Variants = [
-	[new AudioPreset(audios[Math.floor(Math.random() * audios.length)]), new GiphyPreset('Tte95WnDGq5Gg')]
+	[new AudioPreset('591518472'), new GiphyPreset('Tte95WnDGq5Gg')],
+    [new AudioPreset('199105914'), new GiphyPreset('Tte95WnDGq5Gg')],
 ];
