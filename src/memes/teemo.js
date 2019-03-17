6e60be6183209044c878c86f017fb842897da62c
nMusacchio <musacchio.ignacio@gmail.com>
2019-03-17 14:26:27 -0300

    Teemo

--- src/memes/teemo.js
+++ src/memes/teemo.js
@@ -2,6 +2,8 @@ import AudioPreset from '@presets/AudioPreset';
 import CanvasPreset from '@presets/CanvasPreset.js';
 import {GiphyPreset} from '@presets/ImagePreset';
 
+var audios = ['591518472', '199105914'];
+
 export let Variants = [
-	[new AudioPreset('https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/199105914&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'), new GiphyPreset('Tte95WnDGq5Gg')]
+	[new AudioPreset(audios[Math.floor(Math.random() * audios.length)]), new GiphyPreset('Tte95WnDGq5Gg')]
 ];
