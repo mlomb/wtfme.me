488a66f3a569986dce333a48eaff4fd6ce0fcef4
nMusacchio <musacchio.ignacio@gmail.com>
2019-03-18 12:28:26 -0300

    Teemo

--- src/memes/teemo.js
+++ src/memes/teemo.js
@@ -3,11 +3,17 @@ import CanvasPreset from '@presets/CanvasPreset.js';
 import {GiphyPreset} from '@presets/ImagePreset';
 
 const deal = new GiphyPreset('Tte95WnDGq5Gg');
+const deal2 = new GiphyPreset('3eukDb5E6ZNkI');
 
 export let Variants = [
 	[new SoundCloudPreset('591518472'), deal],
 	[new SoundCloudPreset('199105914'), deal],
 	[new SoundCloudPreset('591531243'), deal],
 	[new SoundCloudPreset('591531087'), deal],
-	[new SoundCloudPreset('591531159'), deal]
+	[new SoundCloudPreset('591531159'), deal],
+	[new SoundCloudPreset('591518472'), deal2],
+	[new SoundCloudPreset('199105914'), deal2],
+	[new SoundCloudPreset('591531243'), deal2],
+	[new SoundCloudPreset('591531087'), deal2],
+	[new SoundCloudPreset('591531159'), deal2]
 ];
