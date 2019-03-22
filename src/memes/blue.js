f805ea290a071bd2347747f2b48b214b992ee0fa
nMusacchio <musacchio.ignacio@gmail.com>
2019-03-21 22:54:03 -0300

    Blue fix

--- src/memes/blue.js
+++ src/memes/blue.js
@@ -5,7 +5,7 @@ export let Variants = [
 	[
         new GiphyPreset('ljSSxlYrMA7ss'), new AudioPreset({
             url: `https://bottons.xyz/audio/I'm%20Blue.mp3`,
-            loop: false
+            loop: true
         })
     ]
 ];
