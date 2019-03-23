71c9af709a8eac4e0c337b6871c0046e99cfa98b
Martín Lombardo <mlomb@users.noreply.github.com>
2019-03-23 13:53:49 -0300

    Is it friday modification

--- src/pages/friday.js
+++ src/pages/friday.js
@@ -1,32 +1,39 @@
 import {GiphyPreset} from '@presets/ImagePreset';
 import TextPreset from '@presets/TextPreset';
 
+let day = new Date().getDay();
+let isFriday = day == 5;
 
-let question = new TextPreset({content: "Is it friday?", top: '100px'});
-let yes = new TextPreset({content: "YES", bottom: '100px'});
-let no = new TextPreset({content: "NO", bottom: '100px'});
-
-let friday = [
-    [new GiphyPreset('10hO3rDNqqg2Xe'), yes, question],
-    [new GiphyPreset('3oEjHKjVoNVsCeMoDe'), yes, question],
-    [new GiphyPreset('sTczweWUTxLqg'), yes, question],
-    [new GiphyPreset('b23a67ZhemOe4'), yes, question],
-    [new GiphyPreset('bTveRfBYidENtRBJw9'), yes, question],
-    [new GiphyPreset('yLPJtMLnRiP6w'), yes, question]
-];
-let notFriday = [
-    [new GiphyPreset('o5oLImoQgGsKY'), no, question],
-    [new GiphyPreset('5gUnOrltPvZzW'), no, question],
-    [new GiphyPreset('3o6Zt62PeJeFUDwBUI'), no, question],
-    [new GiphyPreset('34ZNcoaN5u4hi'), no, question],
-    [new GiphyPreset('l2JhpjWPccQhsAMfu'), no, question]
-]
-
-let variants = [];
-if(new Date().getDay() == 5) {
-    variants = friday;
-} else {
-
+let content = 'NO';
+switch(day) {
+	case 4: content = `ALMOST`; break;
+	case 5: content = `<span class="rainbow">YES</span>`; break;
+	case 6: content = `JUST MISSED IT`; break;
 }
 
-export let Variants = variants;
+let question = new TextPreset({content: "Is it friday?", top: '100px'});
+let answer = new TextPreset({
+	content: content,
+	html: true,
+	bottom: '100px'
+});
+
+let friday = [
+    '10hO3rDNqqg2Xe',
+    '3oEjHKjVoNVsCeMoDe',
+    'sTczweWUTxLqg',
+    'b23a67ZhemOe4',
+    'bTveRfBYidENtRBJw9',
+    'yLPJtMLnRiP6w'
+];
+let notFriday = [
+    'o5oLImoQgGsKY',
+    '5gUnOrltPvZzW',
+    '3o6Zt62PeJeFUDwBUI',
+    '34ZNcoaN5u4hi',
+    'l2JhpjWPccQhsAMfu'
+];
+
+export let Variants = (isFriday ? friday : notFriday).map((gif) => {
+	return [new GiphyPreset(gif), question, answer];
+});
