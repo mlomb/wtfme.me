9bb12f9f3a4d0c2afaa2bbb035189d84f30f9a2d
nMusacchio <musacchio.ignacio@gmail.com>
2019-03-25 01:45:31 -0300

    Alf update

--- src/pages/alf.js
+++ src/pages/alf.js
@@ -4,7 +4,6 @@ export let Variants = [
 	new GiphyPreset('xT0BKvuvDLDDYK0fO8'),
     new GiphyPreset('XRKWmpN7NrJhm'),
     new GiphyPreset('LLWteC47MmBck'),
-    new GiphyPreset('NCTdtki7Q7Rew'),
     new GiphyPreset('ToMjGprTg9STlGOcxC8'),
-    new GiphyPreset('HDZqXg5v9HFw4')
+	new GiphyPreset('WmPVwSopE88fu')
 ];
